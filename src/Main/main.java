05b21bba06a90841f259bf379a95e89d50c0ad60
yakiniki <nakagome@tento-net.com>
2017-01-01 22:22:22 +0900

    Update main.java
    これでインポートとかやったらいけるはず...?

--- src/Main/main.java
+++ src/Main/main.java
@@ -9,14 +9,22 @@ import javax.swing.*;
      JFrame frame=new JFrame("KansuEmitter");
      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
      frame.setSize(400,400);
+     JPanel p = new JPanel();
+     JTextField textfield = new JTextField();
+     
+     p.add(textfield);
+    
+     Container contentpane = frame.getContentPane();
+     contentpane.add(p, BorderLayout.CENTER);
+     
      frame.setVisible(true);
     }
-  public void JTextField() {
+/*  public void JTextField() {
      JPanel panelBase = new JPanel();
      JTextField text = new JTextField("パスを入力して下さい");
      text.setMargin(new Insets(5, 10, 5, 10));
      text.setSize(50,10);
      text.setVisible(true);
      String str = text.getText();
-   }
+   } */
 }

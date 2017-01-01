d4527ca04660d396878f24859917900a0e4f2ba5
Kanjyosen <trr95goo@gmail.com>
2017-01-01 16:47:04 +0900

    First Commit. It's ponkotsu yet.

---
+++ src/Main/main.java
@@ -0,0 +1,12 @@
+package Main;
+
+import javax.swing.*;
+ 
+ public class main{
+   public static void main(String args[]){
+     JFrame frame=new JFrame("KansuEmitter");
+     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     frame.setSize(400,400);
+     frame.setVisible(true);
+   }
+}

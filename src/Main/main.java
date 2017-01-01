1e5b1b2711020017da1cb64f0b3eb11922c3ac48
Kanjyosen <trr95goo@gmail.com>
2017-01-01 22:08:25 +0900

    kaizenn

--- src/Main/main.java
+++ src/Main/main.java
@@ -1,5 +1,7 @@
 package Main;
 
+import java.awt.Insets;
+
 import javax.swing.*;
  
  public class main{
@@ -8,7 +10,11 @@ import javax.swing.*;
      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
      frame.setSize(400,400);
      frame.setVisible(true);
+    }
+  public void JTextField() {
+     JPanel panelBase = new JPanel();
      JTextField text = new JTextField("パスを入力して下さい");
+     text.setMargin(new Insets(5, 10, 5, 10));
      text.setSize(50,10);
      text.setVisible(true);
      String str = text.getText();

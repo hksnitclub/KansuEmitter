ef10ada439c5a0a86bff39893116199dee643f0d
電車君 <densyakun1128@gmail.com>
2017-01-06 22:06:08 +0900

    でんでんむし第一号

--- src/Main/main.java
+++ src/Main/main.java
@@ -1,41 +1,6 @@
-package Main;
-
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Insets;
-import java.awt.event.*;
-import javax.swing.*;
-
-import java.awt.Container;
-
- public abstract class main extends JFrame implements ActionListener{
-   public static void main(String args[]){
-     JFrame frame=new JFrame("KansuEmitter");
-     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-     frame.setSize(400,400);
-     JPanel p = new JPanel();
-     JTextField text = new JTextField("パスを入力して下さい");
-     String str = text.getText();
-     text.setPreferredSize(new Dimension(200, 25));
-     text.setVisible(true);	
-     JButton button = new JButton("出力");
-     button.addActionListener(null);
-     java.awt.Container contentpane = frame.getContentPane();
-     contentpane.add(p, BorderLayout.CENTER);
-     frame.setVisible(true);
-     p.add(text);
-     p.add(button);
-    }
-   public void actionPerformed(ActionEvent e){
-	   copyToClipboad("Desktop.getDesktop().open();");
-	   }
-   }
-/*  public void JTextField() {
-     JPanel panelBase = new JPanel();
-     JTextField text = new JTextField("パスを入力して下さい");
-     text.setMargin(new Insets(5, 10, 5, 10));
-     text.setSize(50,10);
-     text.setVisible(true);
-     String str = text.getText();
-   } */
-
+package main;
+public class Main {
+	public static void main(String[] args) {
+		new KEFrame();
+	}
+}
---
+++ src/main/KEFrame.java
@@ -0,0 +1,58 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class KEFrame extends JFrame implements ActionListener {
+	JPanel p = new JPanel();
+	JTextField text = new JTextField("パスを入力して下さい");
+	JButton button = new JButton("出力");
+	public KEFrame() {
+		super("KansuEmitter");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400,400);
+		text.setPreferredSize(new Dimension(200, 25));
+		text.setVisible(true);
+		button.addActionListener(this);
+		getContentPane().add(p, BorderLayout.CENTER);
+		setVisible(true);
+		p.add(text);
+		p.add(button);
+	}
+	public void actionPerformed(ActionEvent e){
+		if (e.getSource() == button) {
+			//ボタンをおした時
+			Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+			Transferable transferable = clipboard.getContents(null);
+			if (transferable != null) {
+				try {
+					text.setText(transferable.getTransferData(DataFlavor.stringFlavor).toString());
+				} catch (UnsupportedFlavorException | IOException e1) {
+					text.setText(e1.getMessage());
+				}
+			}
+		}
+		//copyToClipboad("Desktop.getDesktop().open();");
+	}
+	/*public void JTextField() {
+		JPanel panelBase = new JPanel();
+		JTextField text = new JTextField("パスを入力して下さい");
+		text.setMargin(new Insets(5, 10, 5, 10));
+		text.setSize(50,10);
+		text.setVisible(true);
+		String str = text.getText();
+	}*/
+}
